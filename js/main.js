53791d15d2e0fa0aa406567079e3dcc6483e8322
Joaquin <joaquin.butera@gmail.com>
2024-06-23 00:16:24 -0300

    Working on index

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-const navBar = document.querySelector(".header__menu-container");
-const hamburguerMenu = document.querySelector(".header__menu-toggle");
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  const menuContainer = document.querySelector(".header__menu-container");
+  const hamburguerMenu = document.querySelector(".header__menu-toggle");
 
-hamburguerMenu.addEventListener("click", () => {
-  if (navBar.style.display === "none") navBar.style.display = "flex";
-  else navBar.style.display = "none";
+  const adjustMenuContainer = () => {
+    if (window.innerWidth >= 768) {
+      while (menuContainer.firstChild) {
+        header.appendChild(menuContainer.firstChild);
+      }
+      menuContainer.remove();
+    } else {
+      if (!document.querySelector(".header__menu-container")) {
+        const newMenuContainer = document.createElement("div");
+        newMenuContainer.classList.add("header__menu-container");
+        while (header.children.length > 2) {
+          newMenuContainer.appendChild(header.children[2]);
+        }
+        header.appendChild(newMenuContainer);
+      }
+    }
+  };
+
+  adjustMenuContainer();
+
+  window.addEventListener("resize", adjustMenuContainer);
+
+  hamburguerMenu.addEventListener("click", () => {
+    if (menuContainer.style.display === "none")
+      menuContainer.style.display = "flex";
+    else menuContainer.style.display = "none";
+  });
 });
