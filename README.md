65d88a12960fbcab6a9c417a7b04b9a372f2ee32
Joaquin <joaquin.butera@gmail.com>
2024-06-30 04:16:38 -0300

    Página traducida & más

--- README.md
+++ README.md
@@ -3,22 +3,29 @@
 ## Página web de la empresa de tours personalizados por la ciudad de Buenos Aires Exclusive BA.
 
 ## Tabla de contenidos
+
 1. [Instalación](#instalación)
 2. [Uso](#uso)
 3. [Estructura del Proyecto](#estructura-del-proyecto)
 
 ## Instalación
+
 1. Clonar el repositorio:
-  ```sh
-  git clone https://github.com/Joaquin-5/Exclusive-BA.git
-  ```
+
+```sh
+git clone https://github.com/Joaquin-5/Exclusive-BA.git
+```
+
 2. Acceder al directorio del proyecto:
-  ```sh
-  cd Exclusive BA
-  ```
+
+```sh
+cd Exclusive BA
+```
+
 3. Abrir el archivo index.html
 
 ## Uso
+
 La página ofrece información acerca de tours. Puedes navegar a través de las secciones usando el menú de navegación en la parte superior de la página o en el footer
 
 ## Estructura del proyecto
@@ -28,11 +35,17 @@ Exclusive-BA/
 ├── css/
 │   ├── style.css
 │   └── reset.css
+│   └── lang-selector.css
 ├── img/
 │   └── exclusivebalogowithoutbg.jpg
 │   └── exclusivebalogowithbg.jpeg
 │   └── obelisco.jpg
 ├── js/
 │   └── main.js
+├── translations/
+│   └── translations_en.json
+│   └── translations_es.json
+│   └── translations_pt.json
 ├── index.html
 └── README.md
+```
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector(".header");
   const menuContainer = document.querySelector(".header__menu-container");
   const hamburguerMenu = document.querySelector(".header__menu-toggle");
+  const languageSelector = document.getElementById("languageSelector");
+  const copyright = document.querySelector(".footer__copyright");
 
   // Toggle el div con la clase header__menu-container
   const adjustMenuContainer = () => {
@@ -35,4 +37,36 @@ document.addEventListener("DOMContentLoaded", () => {
       menuContainer.style.display = "flex";
     else menuContainer.style.display = "none";
   });
+
+  // Traductor
+  const loadTranslations = async (lang) => {
+    const response = await fetch(`./translations/translations_${lang}.json`);
+    const translations = await response.json();
+    applyTranslations(translations);
+  };
+
+  const applyTranslations = (translations) => {
+    document.querySelectorAll("[data-translate]").forEach((element) => {
+      const key = element.getAttribute("data-translate");
+      const translation = key
+        .split(".")
+        .reduce((obj, i) => obj[i], translations);
+      if (translation) {
+        element.textContent = translation;
+      }
+    });
+  };
+
+  languageSelector.addEventListener("change", (event) => {
+    const selectedLang = event.target.value;
+    loadTranslations(selectedLang);
+  });
+
+  // Carga el lenguaje
+  loadTranslations(languageSelector.value);
+
+  // Copyright Year
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  copyright.textContent = `© ${currentYear} Exclusive BA. Todos los derechos reservados.`;
 });
