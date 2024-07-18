4b6ae2c8988c27e5367cdc8796414a223efc3d2d
Joaquin <74312560+Joaquin-5@users.noreply.github.com>
2024-07-18 15:46:38 -0300

    arreglando el menu de idiomas

--- js/main.js
+++ js/main.js
@@ -2,58 +2,45 @@ document.addEventListener("DOMContentLoaded", () => {
   const htmlElement = document.documentElement;
   const menuContainer = document.querySelector(".header__menu-container");
   const hamburguerMenu = document.querySelector(".header__menu-toggle");
-  const languageSelectorDesktop = document.getElementById(
-    "languageSelectorDesktop"
-  );
-  const languageSelectorMobile = document.getElementById(
-    "languageSelectorMobile"
-  );
-  const selectTrigger = document.querySelector('.custom-select__trigger');
-  const customOptions = document.querySelector('.custom-select__options');
-  const customOptionsList = document.querySelectorAll('.custom-select__option');
+  const languageSelectors = document.querySelectorAll(".custom-select__wrapper");
+  const selectTriggers = document.querySelectorAll(".custom-select__trigger");
+  const customOptions = document.querySelectorAll(".custom-select__options");
+  const customOptionsList = document.querySelectorAll(".custom-select__option");
   const reviewsContainer = document.querySelector(".reviews-section");
   const copyrightElement = document.querySelector(".footer__copyright");
 
   // Toggle menú hamburguesa
   hamburguerMenu.addEventListener("click", () => {
-    if (
-      menuContainer.style.display === "none" ||
-      menuContainer.style.display === ""
-    )
-      menuContainer.style.display = "flex";
-    else menuContainer.style.display = "none";
+    menuContainer.style.display = (menuContainer.style.display === "none" || menuContainer.style.display === "") ? "flex" : "none";
   });
 
-  selectTrigger.addEventListener('click', function() {
-    customOptions.classList.toggle('custom-select__options--open');
-});
-
-customOptionsList.forEach(option => {
-    option.addEventListener('click', function() {
-        selectTrigger.querySelector('.custom-select__label').innerHTML = this.innerHTML;
-        customOptions.querySelector('.custom-select__option--selected').classList.remove('custom-select__option--selected');
-        this.classList.add('custom-select__option--selected');
-        customOptions.classList.remove('custom-select__options--open');
-        
-        // Actualiza el valor del selector de idioma
-        const selectedLang = this.getAttribute('data-value');
-        handleLanguageChange(selectedLang);  // Llama a la función para cambiar el idioma
+  selectTriggers.forEach(trigger => {
+    trigger.addEventListener('click', function() {
+      this.nextElementSibling.classList.toggle('custom-select__options--open');
     });
-});
-
-document.addEventListener('click', function(e) {
-    if (!selectTrigger.contains(e.target)) {
-        customOptions.classList.remove('custom-select__options--open');
-    }
-});
-
-  document.addEventListener("click", (e) => {
-    if (!selectTrigger.contains(e.target)) {
-      customOptions.classList.remove("custom-select__options--open");
-    }
   });
 
-  // Traductor
+  customOptionsList.forEach(option => {
+    option.addEventListener('click', function() {
+      const trigger = this.closest('.custom-select').querySelector('.custom-select__trigger .custom-select__label');
+      trigger.innerHTML = this.innerHTML;
+      this.closest('.custom-select__options').querySelector('.custom-select__option--selected').classList.remove('custom-select__option--selected');
+      this.classList.add('custom-select__option--selected');
+      this.closest('.custom-select__options').classList.remove('custom-select__options--open');
+      
+      const selectedLang = this.getAttribute('data-value');
+      handleLanguageChange(selectedLang);
+    });
+  });
+
+  document.addEventListener('click', function(e) {
+    customOptions.forEach(option => {
+      if (!option.previousElementSibling.contains(e.target)) {
+        option.classList.remove('custom-select__options--open');
+      }
+    });
+  });
+
   const loadTranslations = async (lang) => {
     try {
       const response = await fetch(`./translations/translations_${lang}.json`);
@@ -69,48 +56,36 @@ document.addEventListener('click', function(e) {
   const applyTranslations = (translations) => {
     document.querySelectorAll("[data-translate]").forEach((element) => {
       const key = element.getAttribute("data-translate");
-      const translation = key
-        .split(".")
-        .reduce((obj, i) => obj[i], translations);
+      const translation = key.split(".").reduce((obj, i) => obj[i], translations);
       if (translation) {
-        // Reemplaza \n\n por <br><br> si el elemento es un párrafo
-        if (element.tagName.toLowerCase() === "p") {
-          element.innerHTML = translation.replace(/\n\n/g, "<br><br>");
-        } else {
-          element.textContent = translation;
-        }
+        element.innerHTML = (element.tagName.toLowerCase() === "p") ? translation.replace(/\n\n/g, "<br><br>") : translation;
       }
     });
   };
 
-  // Manejar el cambio de idioma y persistir la selección
   const handleLanguageChange = (lang) => {
     localStorage.setItem("selectedLang", lang);
     loadTranslations(lang);
-    languageSelectorDesktop.value = lang;
-    languageSelectorMobile.value = lang;
-    document.documentElement.lang = lang; // Cambia el atributo lang en la etiqueta html
+    updateSelectors(lang);
   };
 
-  // Cargar el idioma persistido o usar un valor por defecto
+  const updateSelectors = (lang) => {
+    languageSelectors.forEach(selector => {
+      const options = selector.querySelectorAll('.custom-select__option');
+      options.forEach(option => {
+        if (option.getAttribute('data-value') === lang) {
+          option.classList.add('custom-select__option--selected');
+          option.closest('.custom-select').querySelector('.custom-select__label').innerHTML = option.innerHTML;
+        } else {
+          option.classList.remove('custom-select__option--selected');
+        }
+      });
+    });
+  };
+
   const persistedLang = localStorage.getItem("selectedLang") || "es";
-  languageSelectorDesktop.value = persistedLang;
-  languageSelectorMobile.value = persistedLang;
-  loadTranslations(persistedLang);
+  handleLanguageChange(persistedLang);
 
-  // Añadir un evento de cambio a ambos selectores de idioma
-  languageSelectorDesktop.addEventListener("change", (event) => {
-    const selectedLang = event.target.value;
-    handleLanguageChange(selectedLang);
-  });
-
-  languageSelectorMobile.addEventListener("change", (event) => {
-    const selectedLang = event.target.value;
-    handleLanguageChange(selectedLang);
-  });
-
-  // fetch de las reseñas
-  // Asumiendo que el archivo JSON está en la ruta '/data/reviews.json'
   if (document.URL.includes("resenas.html")) {
     fetch("/data/reviews.json")
       .then((response) => response.json())
@@ -118,21 +93,13 @@ document.addEventListener('click', function(e) {
         reviews.forEach((review) => {
           let starsHTML = "";
           for (let i = 0; i < 5; i++) {
-            if (i < review.stars) {
-              starsHTML += '<i class="fa fa-star checked"></i>';
-            } else {
-              starsHTML += '<i class="fa fa-star"></i>';
-            }
+            starsHTML += (i < review.stars) ? '<i class="fa fa-star checked"></i>' : '<i class="fa fa-star"></i>';
           }
-  
+
           const reviewHTML = `
             <div class="review-card">
               <div class="review-card__header">
-              ${
-                review.image
-                  ? `<img src="${review.image}" alt="${review.name}" class="review-card__image" />`
-                  : `<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png" alt="${review.name}" class="review-card__image" />`
-              }
+              ${review.image ? `<img src="${review.image}" alt="${review.name}" class="review-card__image" />` : `<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png" alt="${review.name}" class="review-card__image" />`}
                 <div class="review-card__details">
                   <h2 class="review-card__name">${review.name}</h2>
                   <div class="review-card__stars">${starsHTML}</div>
@@ -140,9 +107,7 @@ document.addEventListener('click', function(e) {
               </div>
               <hr class="review-card__separator" />
               <div class="review-card__content">
-                <p>
-                  ${review.description}
-                </p>
+                <p>${review.description}</p>
               </div>
             </div>
           `;
@@ -152,7 +117,6 @@ document.addEventListener('click', function(e) {
       .catch((error) => console.error("Error fetching reviews:", error));
   }
 
-  // Actualizar el copyright con el año actual
   const updateCopyright = (copyrightText) => {
     const year = new Date().getFullYear();
     if (copyrightElement && copyrightText) {
